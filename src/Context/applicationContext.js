0d44ebbd2a0093bb4e82f7acbf16ee7688774f69
BOSTONE069 <bostoneochieng69@gmail.com>
2023-11-10 11:09:03 +0300

    Created the application context provide to manage user data stored in cookies using the useCookies hook

---
+++ src/Context/applicationContext.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useCookies } from "react-cookie";
+
+const AppContext = React.createContext();
+
+const AppContextProvider = ({ children }) => {
+    const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
+
+    //Used to set UserData(username and token) to cookies
+    const setSession = (userData) => {
+        setCookie("userData", userData, {
+            path: "/",
+            maxAge: 900, //15 minutes
+        });
+    };
+
+    const getSession = () => {
+        const userData = cookies.userData || null;
+        return userData;
+    };
+
+    const logout = () => {
+        removeCookie("userData", { path: "/"});
+    };
+
+    return (
+        <AppContext.Provider
+            value={{
+                setSession,
+                getSession,
+                logout,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export default AppContextProvider;
