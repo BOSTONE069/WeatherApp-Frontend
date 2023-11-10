d45e34d4d6e9667141c17beb6518bace6ca29b9c
BOSTONE069 <bostoneochieng69@gmail.com>
2023-11-10 11:38:49 +0300

    Adde the toaster into the application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
 import AppContextProvider from './Context/applicationContext';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +13,15 @@ root.render(
      <Router>
       <App />
     </Router>
+    <Toaster
+      toastOptions={{
+        duration: 5000,
+        style: {
+          background: "#7e0bdb",
+          color: "#fff",
+        }
+      }}
+    />
   </AppContextProvider>
  
     
